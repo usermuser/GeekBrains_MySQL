8ed7b443d266cbe3dc7cc4f85aa8ef35f05426f4
usermuser <lwrdrstv@gmail.com>
2018-12-26 22:13:13 +0500

    avg done

--- les_3_hw/hw3_AVG.sql
+++ les_3_hw/hw3_AVG.sql
@@ -1,7 +1,7 @@
 # База данных «Сотрудники»:
 # 1. Выбрать среднюю зарплату по отделам.
 
-SELECT SUM(`zp`), `depart`.`name` FROM `shtat` 
+SELECT AVG(`zp`) AS 'Средняя ЗП', `depart`.`name` AS 'Отдел' FROM `shtat` 
 LEFT JOIN `depart`
 ON `depart`.`id` = `shtat`.`otdel`
 GROUP BY `otdel`;
