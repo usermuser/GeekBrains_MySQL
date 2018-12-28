4702b5ea52048a93f92ea14785f979079711cdd1
usermuser <lwrdrstv@gmail.com>
2018-12-28 13:09:08 +0500

    merged files into one file

--- les_4_hw/hw4.1.sql
+++ les_4_hw/hw4.1.sql
@@ -17,3 +17,86 @@ CREATE VIEW `view_hw4.1` AS
     
 # После этого делаем проверочную выборку
 select * from `view_hw4.1`;
+
+
+# ============================================
+
+# 2. Выбрать все города из Московской области.
+USE GB_schema;
+
+CREATE VIEW `VIEW_4.2` AS
+
+	SELECT `_cities`.`title` AS 'Город', 
+		   `_countries`.`title` AS 'Страна',
+		   `_regions`.`title` AS 'Область'
+	FROM `_cities`
+
+	LEFT JOIN `_countries`
+	ON `_countries`.`id` = `_cities`.`country_id`
+
+	LEFT JOIN `_regions`
+	ON `_regions`.`id` = `_cities`.`region_id`
+
+	WHERE `_regions`.`title` = 'Московская ';
+
+
+# ============================================
+
+
+# 1. Выбрать среднюю зарплату по отделам.
+USE `sotrudniki`;
+
+CREATE VIEW `view_get_avg_zp_by_depart` AS
+
+	SELECT AVG(`zp`) AS 'Средняя ЗП', `depart`.`name` AS 'Отдел' FROM `shtat` 
+	LEFT JOIN `depart`
+	ON `depart`.`id` = `shtat`.`otdel`
+	GROUP BY `otdel`;
+
+
+# ============================================
+
+
+# Потренируемся создавать процедуры
+
+use `sotrudniki`;
+delimiter //
+# Создадим процедуру
+CREATE DEFINER=`root`@`localhost` PROCEDURE `find_manager`(OUT param1 varchar(25))
+BEGIN
+  SELECT COUNT(*) INTO param1 FROM `sotrudniki`.`shtat`;
+END//
+
+delimiter ;
+# Вызовем процедуру
+call `find_manager`(@a);
+select @a as 'ФИО';
+
+
+# ============================================
+
+delimiter //
+# Создадим еще одну процедуру
+CREATE PROCEDURE proc_test(OUT par1 INT)
+    -> BEGIN
+    -> SELECT MAX(`zp`) INTO par1 FROM `shtat` LIMIT 1;
+    -> END//
+
+delimiter ;
+CALL `proc_test` (@b);
+
+SELECT @b;
++-------+
+| @b    |
++-------+
+| 23000 |
++-------+
+1 row in set (0,00 sec)
+
+
+# ============================================
+
+
+# 2. Создать функцию, которая найдет менеджера по имени и фамилии.
+
+
