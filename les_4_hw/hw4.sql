48b82bcdba8227ddc111903b5bdc447f5a2a67c2
usermuser <lwrdrstv@gmail.com>
2018-12-31 00:38:34 +0500

    hw 4 done

--- les_4_hw/hw4.sql
+++ les_4_hw/hw4.sql
@@ -116,3 +116,57 @@ mysql> SELECT `get_manager_id_by_name` ('Anton') as `id`;
 |    1 |
 +------+
 1 row in set (0,00 sec)
+
+
+# ====================================================
+
+# Создадим базу для триггера
+mysql> USE `sotrudniki`; 
+
+mysql> CREATE TABLE `salary` ( 
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	`sotr_id` INT, 
+	`income` INT 
+);
+Query OK, 0 rows affected (0,39 sec)
+
+# создадим триггер
+mysql> CREATE TRIGGER `trig_bonus` 
+AFTER INSERT ON `shtat` 
+FOR EACH ROW  
+INSERT INTO `salary` (`sotr_id`, `income`) VALUES (NEW.id, 10000);
+Query OK, 0 rows affected (0,12 sec)
+
+# проверим триггер
+INSERT INTO `sotrudniki`.`shtat` 
+(`zp`, `name`, `lastname`, `otdel`) VALUES 
+('7777', 'Волков', 'Коммандер', '2');
+# видим что добавился сотрудник 
+Волков Коммандер с id 11 и в таблице
+salary появился бонус для сотрудника с id 11
+salary появился бонус для сотрудника с id 11
+
+mysql> SELECT * FROM `sotrudniki`.`shtat`
+    -> ;
++----+------------------+----------------------+-------+--------+
+| id | name             | lastname             | otdel | zp     |
++----+------------------+----------------------+-------+--------+
+|  1 | Тарас            | Голомозый            |     1 |  25000 |
+|  2 | Андрей           | Юрченко              |     1 |  20000 |
+|  4 | Антон            | Полонников           |     2 |  27000 |
+|  5 | Анатолий         | Башлыков             |     1 |  25000 |
+|  7 | Аркадий          | Паровозов            |     1 |   1500 |
+|  8 | Вася             | Левченко             |     4 |  30000 |
+|  9 | 123              | 123                  |     1 | 123123 |
+| 10 | Ваваыв           | авыфа                |     2 | 222222 |
+| 11 | Волков           | Коммандер            |     2 |   7777 |
++----+------------------+----------------------+-------+--------+
+9 rows in set (0,00 sec)
+
+mysql> SELECT * FROM `sotrudniki`.`salary`;
++----+---------+--------+
+| id | sotr_id | income |
++----+---------+--------+
+|  1 |      11 |  10000 |
++----+---------+--------+
+1 row in set (0,00 sec)
